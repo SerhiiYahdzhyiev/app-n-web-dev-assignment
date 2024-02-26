f728286336fbef4f337ab1f419e39a23350c9170
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-02-26 12:26:12 +0100

    Added defaults to config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,10 @@
 import {config} from "dotenv";
+import { Defaults } from "./defaults";
 
 config();
 
-const port: number = +process.env.DB_PORT!;
-const host: string = process.env.DB_HOST!;
+const port: number = parseInt(process.env.DB_PORT!) ?? Defaults.db.PORT;
+const host: string = process.env.DB_HOST ?? Defaults.db.HOST;
 const dbName: string = process.env.DB_NAME!;
 const user: string = process.env.DB_USER!;
 const pass: string = process.env.DB_PASSWORD!;
