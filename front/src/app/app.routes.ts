be3299f5b5e26385b77f5450e045a09ccb06e1b0
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-01 11:22:20 +0200

    Added base pages to router

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { LandingPage, NotFoundPage } from "@pages";
+
+export const routes: Routes = [
+  { path: "", component: LandingPage },
+  { path: "**", component: NotFoundPage },
+];
