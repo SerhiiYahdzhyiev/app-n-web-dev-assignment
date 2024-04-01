f9ccf13b14d2beccfd4ff2770a8a04f5b5331da4
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-01 15:41:44 +0200

    Added User interface

--- front/src/interfaces/user.interface.ts
+++ front/src/interfaces/user.interface.ts
@@ -1,5 +1,11 @@
+export enum UserRoles {
+  ADMIN = "ADMIN",
+  CUSTOMER = "CUSTOMER",
+}
+
 export interface IUser {
   firstName: string;
   lastName: string;
   email: string;
+  role: UserRoles;
 }
