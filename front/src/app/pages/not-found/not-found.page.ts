cf273eb38f6259973bde4da3ae8dd5746368a2d0
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-01 11:20:22 +0200

    Created not found page

---
+++ front/src/app/pages/not-found/not-found.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "not-found",
+  templateUrl: "./not-found.page.html",
+  styleUrl: "./not-found.page.css",
+})
+export class NotFoundPage { }
