dee9eaeec147c594cdd62554b9e6a6b41a845e59
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-01 15:40:43 +0200

    Added loader page

--- front/src/app/pages/index.ts
+++ front/src/app/pages/index.ts
@@ -1,3 +1,4 @@
 export * from "./not-found/not-found.page";
 export * from "./landing/landing.page";
 export * from "./admin/admin.page";
+export * from "./loader/loader.page";
---
+++ front/src/app/pages/loader/loader.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "loader-page",
+  standalone: true,
+  templateUrl: "./loader.page.html",
+  styleUrl: "./loader.page.css",
+})
+export class LoaderPage {}
