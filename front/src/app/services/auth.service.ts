854a6b444ca66ba7174820df65c4b0ead5b0f7da
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-01 15:37:55 +0200

    Added auth service

---
+++ front/src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+
+import { ApiService } from "./api.service";
+
+@Injectable()
+export class AuthService {
+  constructor(private client: ApiService) {}
+
+  login(login: string, password: string) {
+    return this.client.post(
+      "/auth/login",
+      { login, password },
+    );
+  }
+}
