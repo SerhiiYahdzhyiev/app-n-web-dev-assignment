88da8dcb921ac424fcf23abcc003f985564ee823
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-02 20:01:02 +0200

    Extended user interface

--- front/src/interfaces/user.interface.ts
+++ front/src/interfaces/user.interface.ts
@@ -4,8 +4,15 @@ export enum UserRoles {
 }
 
 export interface IUser {
+  id: string;
   firstName: string;
   lastName: string;
   email: string;
+  phoneNumber: string;
+  countryCode: string;
+  deliveryAddress: string;
   role: UserRoles;
+
+  createdAt: string;
+  updatedAt: string;
 }
