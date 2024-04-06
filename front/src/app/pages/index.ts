a9924864ac9fba037d5a046ea207ec10bda23a3c
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-06 21:10:45 +0200

    Added dummy products page

--- front/src/app/pages/index.ts
+++ front/src/app/pages/index.ts
@@ -2,3 +2,5 @@ export * from "./not-found/not-found.page";
 export * from "./landing/landing.page";
 export * from "./admin/admin.page";
 export * from "./loader/loader.page";
+export * from "./products/products.page";
+export * from "./login/login.page";
---
+++ front/src/app/pages/products/products.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "products-page",
+  standalone: true,
+  templateUrl: "./products.page.html",
+})
+export class ProductsPage {
+
+}
