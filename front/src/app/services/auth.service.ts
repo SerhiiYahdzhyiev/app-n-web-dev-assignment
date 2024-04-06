339a845bbdd87340b6c9236ffab63d37f745d3be
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-06 21:11:27 +0200

    Added register method to auth service

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from "@angular/core";
 
 import { ApiService } from "./api.service";
+import { IUser } from "@interfaces";
 
 @Injectable()
 export class AuthService {
-  constructor(private client: ApiService) {}
+  constructor(private client: ApiService) { }
 
   login(login: string, password: string) {
     return this.client.post(
       "/auth/login",
       { login, password },
     );
+  }
+
+  register(payload: Partial<IUser>) {
+    return this.client.post("auth/register", payload);
   }
 
   logout() {
