c3944ed296868d71b779b4e3ff1070aa7897037e
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-06 21:12:10 +0200

    Added new routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from "@angular/router";
 
 import { LoginPage, AdminPage, LandingPage, NotFoundPage, ProductsPage } from "@pages";
+
+
 export const routes: Routes = [
   { path: "", redirectTo: "landing", pathMatch: "full" },
   { path: "landing", component: LandingPage },
   { path: "admin", component: AdminPage },
   { path: "login", component: LoginPage },
+  { path: "register", component: LoginPage },
+  { path: "products", component: ProductsPage },
   { path: "**", component: NotFoundPage },
 ];
