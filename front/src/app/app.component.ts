131e17f5ffbc143ded3ecf97acdc11227f415c61
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-06 21:11:59 +0200

    Added header, mobile wall, and some styles to main app component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,21 +2,27 @@ import { CommonModule } from "@angular/common";
 import { Component, HostListener, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
-import { MobileWallComponent } from "@components";
+import { HeaderComponent, MobileWallComponent } from "@components";
 
 @Component({
   selector: "app-root",
+  host: {
+    "[style.min-height]": "'100vh'",
+    "[style.display]": "'block'",
+  },
   standalone: true,
   templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
   imports: [
     CommonModule,
     RouterOutlet,
     MobileWallComponent,
+    HeaderComponent,
   ],
 })
 export class AppComponent implements OnInit {
   innerWidth: number = 0;
-  minViewportWidth = 600;
+  minViewportWidth = 1000;
 
 
   ngOnInit() {
