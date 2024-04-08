1e040ba9a01a8cb95492b8f18536b72b0915df3e
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-04-08 17:21:35 +0200

    Extended app config with some providers

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { Router, provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { HttpClientModule } from "@angular/common/http";
+import { BasketService } from "./services/basket.service";
+import { ApiService } from "./services/api.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    importProvidersFrom(Router),
     importProvidersFrom(HttpClientModule),
+    importProvidersFrom(BasketService),
+    importProvidersFrom(ApiService),
   ],
 };
