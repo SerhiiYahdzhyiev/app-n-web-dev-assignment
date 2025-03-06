470cc4c97744e42a414b60f7a1e2d3275521568e
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-03-06 16:01:07 +0100

    Add minimalistic caching to recommender service's main handle.

--- recommender/src/app.ts
+++ recommender/src/app.ts
@@ -1,5 +1,8 @@
+import Redis from "ioredis";
 import express, { Express, json } from "express";
 import cors from "cors";
+
+import { cacheConfig } from "./config/cache";
 
 import {
   jsonMiddlewareOptions,
@@ -11,6 +14,15 @@ import { handleError } from "./common/middlewares/error.middleware";
 import {getRecommendedProducts} from "./modules/recommend";
 
 const app: Express = express();
+const cache = new Redis({
+  host: cacheConfig.host,
+  port: cacheConfig.port,
+
+})
+
+cache.on("error", (err) => {
+  logger.error(String(err), {label: "cache"});
+});
 
 app.use(cors({
   origin: "http://localhost:4818",
@@ -19,8 +31,21 @@ app.use(json(jsonMiddlewareOptions));
 
 app.get("/:userId", async (req, res, next) => {
   try {
-    const products = await getRecommendedProducts(req.params.userId);
-    logger.info("Returning recommended products...", {label: "recommender"});
+    const userId = req.params.userId;
+
+    const cacheKey = `rec:${userId}`;
+    const cachedData = await cache.get(cacheKey);
+
+    let products;
+    if (cachedData) {
+      logger.info("Returning cached recommended products...", {label: "recommender"});
+      products = JSON.parse(cachedData);
+    } else {
+      products = await getRecommendedProducts(userId);
+      await cache.set(cacheKey, JSON.stringify(products), "EX", cacheConfig.exp)
+      logger.info("Returning recommended products...", {label: "recommender"});
+    }
+
     res.status(200);
     res.json(products);
   } catch (err) {
