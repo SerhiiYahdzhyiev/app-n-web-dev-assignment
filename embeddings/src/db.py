60a72aa99f2cb8db5722a6610c0de595fada85ab
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-03-10 19:09:21 +0100

    Fix collection clearing for Qdrant in embeddings service.

--- embeddings/src/db.py
+++ embeddings/src/db.py
@@ -9,7 +9,10 @@ QD_PORT = os.environ.get("QD_PORT")
 
 client = QdrantClient(QD_HOST, port=QD_PORT)
 
-client.recreate_collection(
-    collection_name="products",
-    vectors_config={"size": 384, "distance": "Cosine"},
-)
+collection = client.get_collection("products")
+
+if not collection:
+    client.create_collection(
+        collection_name="products",
+        vectors_config={"size": 384, "distance": "Cosine"},
+    )
