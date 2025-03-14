352e4167e6c8c3e6d9556272bbab907216ba1e41
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-03-14 17:18:49 +0100

    Remove unneeded obsolete scripts and configs.

--- get-passwd.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-import { config } from "dotenv";
-
-config();
-
-const salt = +process.env.PSWD_HASH_SALT;
-const pwd = process.env.INIT_ADMIN_PASSWORD;
-
-console.log(bcrypt.hashSync(pwd, salt));
