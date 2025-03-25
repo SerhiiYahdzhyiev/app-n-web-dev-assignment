b90064718fc9ae9039be2ca2a92e36bb4c8ea0e8
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-03-25 19:22:09 +0100

    Integrated embeddings search into recommender service.

--- recommend/src/modules/recommend.ts
+++ recommend/src/modules/recommend.ts
@@ -18,46 +18,6 @@ async function getFallbackProducts() {
   ]).then(res => Product.find({ _id: { $in: res.map(p => p._id) } }));
 }
 
-async function getRecommendedProductsBy(similarUserIds: mongoose.Types.ObjectId[]) {
-  return await Order.aggregate([
-    { $match: { userId: { $in: similarUserIds }, status: OrderStatus.DELIVERED } },
-    { $unwind: "$productsIds" },
-    { $group: { _id: "$productsIds", count: { $sum: 1 } } },
-    { $sort: { count: -1 } },
-    { $limit: K }
-  ]);
-}
-
-async function getOtherUsersOrders(userId: mongoose.Types.ObjectId) {
-  return await Order.aggregate([
-    {$match: {userId: {$ne:userId},status:OrderStatus.DELIVERED}},
-    {$group: {_id: "$userId", products: {$push: "$productsIds"}}},
-  ])
-}
-
-function getJaccardSimilarity(a: Set<any>, b: Set<any>) {
-  const intersection = new Set([...a].filter(i => b.has(i)));
-  const union = new Set([...a, ...b]);
-
-  const similarity = intersection.size / union.size;
-
-  //TODO: Add extra validations... ?
-
-  return similarity
-}
-
-//TODO: Improve typing or arguments...
-function getJaccardSimilarities(otherOrders: any[], userProducts: Set<string>) {
-  return otherOrders
-      .map(({ _id, products }) => {
-        const otherProducts = new Set(products.flat().map(String));
-        const similarity = getJaccardSimilarity(otherProducts, userProducts);
-        return { userId: _id.toString(), similarity };
-      })
-      .sort((a, b) => b.similarity - a.similarity)
-      .slice(0, K);
-}
-
 export async function getRecommendedProducts(userId: string) {
   const uid = new mongoose.Types.ObjectId(userId);
 
@@ -65,21 +25,40 @@ export async function getRecommendedProducts(userId: string) {
 
   if (!userOrders.length) return await getFallbackProducts();
 
-  const userProducts = new Set(
-    userOrders.flatMap((order) => order.productsIds.map(String))
-  );
+  const topProductIds = await Order.aggregate([
+    { $match: { status: OrderStatus.DELIVERED } },
+    { $unwind: "$productsIds" },
+    { $group: { _id: "$productsIds", count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+    { $limit: 1 }
+  ]);
 
-  const otherOrders = await getOtherUsersOrders(uid);
+  const topProduct = await Product.findOne({ _id: topProductIds[0]._id });
 
-  if (!otherOrders.length) return await getFallbackProducts();
+  if (!topProduct) return await getFallbackProducts();
 
-  const similarities = getJaccardSimilarities(otherOrders, userProducts);
-  const similarUserIds = similarities.map(
-    s => new mongoose.Types.ObjectId(s.userId)
-  );
+  const payload = {
+    text: topProduct?.category +
+      " " + topProduct.description +
+      " " + topProduct.title,
+    k: K,
+  };
+  
+  const res = await fetch("http://embed:3096/search", {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
 
-  const recommendedIds = await getRecommendedProductsBy(similarUserIds);
-  const recommendedProducts = await Product.find({_id: {$in: recommendedIds}});
+  if (res.status !== 200) return await getFallbackProducts();
 
+  const data = await res.json();
+  if (!data || !data.results.length) return await getFallbackProducts();
+
+  const ids = data.results.map((i: {id: string, distance: number}) => i.id);
+
+  const recommendedProducts = await Product.find({_id: {$in: ids}});
   return recommendedProducts;
 }
